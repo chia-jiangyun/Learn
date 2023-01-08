99e1cc2c6b21e19ab56826b973bc221feec2c8c7
chia-jiangyun <93819476+chia-jiangyun@users.noreply.github.com>
2023-01-08 15:41:18 +0800

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "Hello World, Thanks you making me very very happy";
+    String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
